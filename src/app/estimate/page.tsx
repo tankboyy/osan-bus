1823f8be5fc3753e9078135df90a67f3e5a631a9
tankboyy <tankboya@naver.com>
2024-12-08 20:03:03 +0900

    feat: 오류 수정

--- src/app/estimate/page.tsx
+++ src/app/estimate/page.tsx
@@ -3,15 +3,6 @@ import EstimateTable from "@/component/estimate/EstimateTable";
 
 
 export default async function Page() {
-	// 견적문의 데이터 예시
-	const estimates = [
-		{id: 1, title: "견적문의 1", date: "2024-11-01", status: "처리중"},
-		{id: 2, title: "견적문의 2", date: "2024-11-05", status: "완료"},
-		{id: 3, title: "견적문의 3", date: "2024-11-10", status: "대기중"},
-	];
-
-	// 예약 데이터 가져오기
-
 	return (
 		<main className="p-8 bg-gray-100 min-h-screen">
 			<EstimateTable/>
@@ -26,29 +17,3 @@ export default async function Page() {
 		</main>
 	);
 }
-
-// 상태에 따라 색상 변경
-function getStatusColor(status) {
-	switch (status) {
-		case "처리중":
-			return "text-yellow-500";
-		case "완료":
-			return "text-green-500";
-		case "대기중":
-			return "text-red-500";
-		default:
-			return "text-gray-500";
-	}
-}
-
-
-import Reserve from "@/component/Reserve";
-//
-// export default function Page() {
-// 	return (
-// 		<>
-// 			<Reserve/>
-//
-// 		</>
-// 	);
-// }
